ff13b7592199e77d9e8a7948ff5acab43ff0b053
soynog <nicolas.o.garcia@gmail.com>
2016-04-06 10:32:13 -0400

    Clean up tictactoe.js
    - Remove extraneous code from previous iterations

--- assets/scripts/game/tictactoe.js
+++ assets/scripts/game/tictactoe.js
@@ -1,16 +1,6 @@
 'use strict';
 
-// Game Object Constructor (new game)
-// const Game = function (playerO, playerX) {
-//   this.cells = ['','','','','','','','',''];
-//   this.playerX = playerX;
-//   this.playerO = playerO;
-//   this.over = false;
-//   this.turn = 'X';
-// };
-
-// Game.prototype place function: adds an o or x to an index. First checks to make sure position is empty. Returns true if successful, false if not.
-const validMove = function (game, index, player) {
+const validMove = function (game, index) {
   if (!game.cells[index]) {
     // game.cells[index] = player;
     // $('button#' + index).text(player);
@@ -18,25 +8,6 @@ const validMove = function (game, index, player) {
   }
   return false;
 };
-
-// Returns an array of index triplets of rows that include the passed index.
-// const rows = function(index) {
-//   let triplets = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-//   return triplets.filter(trip => trip.some(x => x === Number(index)));
-// };
-
-// Game.prototype check win: checks if there are three in a row including the passed index. If there's a winner, return their symbol. If not, return false.
-// const checkWin = function (index) {
-//   let triplets = rows(index);
-//   for (let i in triplets) {
-//     let c = this.cells;
-//     let t = triplets[i];
-//     if (c[t[0]] && c[t[0]] === c[t[1]] && c[t[1]] === c[t[2]]) {
-//         return c[t[0]];
-//     }
-//   }
-//   return false;
-// };
 
 // Check-Win function that takes a board of cells and checks for a win.
 const checkWin = function (game) {
@@ -55,27 +26,6 @@ const checkWin = function (game) {
 const checkFull = function(game) {
   return game.cells.every(i => i);
 };
-
-// Game.prototype turn function: gets user input, makes a place switches whose turn it is, checks win, checks if board is full
-// const playTurn = function(index) {
-//   if(!this.over) {
-//     if(place.call(this,index,this.turn)) {
-//       if(checkWin.call(this,index)) {
-//         $('.announce').text('Game Over! Player ' + this.turn + ' wins!!!');
-//         this.over = true;
-//       } else if (checkFull.call(this)) {
-//         $('.announce').text("Game Over! It's a tie.");
-//         this.over = true;
-//       } else {
-//         this.turn = this.turn === 'X' ? 'O' : 'X';
-//         $('.announce').text("Player " + this.turn + "'s turn.");
-//         // Next turn
-//       }
-//     } else {
-//       $('.announce').text("Invalid Move! Try again, player " + this.turn + ".");
-//     }
-//   }
-// };
 
 // Determine player based on # of cells of each type
 const turn = function(game) {
@@ -112,12 +62,6 @@ const playTurn = function(game,index) {
     }
   }
 };
-
-// Game.prototype end function: ends game, prints a message
-//
-// Game.prototype.playTurn = playTurn;
-// Game.prototype.checkWin = checkWin;
-// Game.prototype.place = place;
 
 module.exports = {
   checkWin,
